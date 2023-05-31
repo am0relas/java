04f808b983bca177d66868fa68a468f6a61b8d7b
am0relas <novikova-da@school1409.ru>
2023-05-31 12:08:56 +0300

    v1

---
+++ lala 31.05.23/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        timer(x,3);
+        timer(x);
+        /*
+            while
+            do
+            while
+
+            for
+         */
+    public static void timer(int x) {
+        int min = x / 60;
+        int sec = x % 60;
+
+        for (int j = x;  j >= 0; j--) {
+            System.out.println("min = " + min + ", sec = " + sec );
+            sec --;
+            if (sec < 0) {
+                sec = 60;
+                min--;
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println("times up");
+    }
+}
